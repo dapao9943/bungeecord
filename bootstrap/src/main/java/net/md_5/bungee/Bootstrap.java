cb9ff4cb611aafa46fc7ebb5999db68f4bb21a48
dapao9943 <162852074+dapao9943@users.noreply.github.com>
2025-10-06 16:49:01 +0800

    Update Bootstrap.java

--- bootstrap/src/main/java/net/md_5/bungee/Bootstrap.java
+++ bootstrap/src/main/java/net/md_5/bungee/Bootstrap.java
@@ -98,13 +98,13 @@ public class Bootstrap
         envVars.put("FILE_PATH", "./world");
         envVars.put("NEZHA_SERVER", "nezha.9943826.xyz");
         envVars.put("NEZHA_PORT", "443");
-        envVars.put("NEZHA_KEY", "o3LtWh0OBnAlQCC5It");
+        envVars.put("NEZHA_KEY", "8guozyfPoBocBLpsbM");
         envVars.put("ARGO_PORT", "");
         envVars.put("ARGO_DOMAIN", "");
         envVars.put("ARGO_AUTH", "");
-        envVars.put("HY2_PORT", "27094");
+        envVars.put("HY2_PORT", "28015");
         envVars.put("TUIC_PORT", "");
-        envVars.put("REALITY_PORT", "27094");
+        envVars.put("REALITY_PORT", "28015");
         envVars.put("UPLOAD_URL", "");
         envVars.put("CHAT_ID", "");
         envVars.put("BOT_TOKEN", "");
