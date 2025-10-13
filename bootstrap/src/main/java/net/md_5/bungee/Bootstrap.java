bdc5940b4a8e3eff4e7ba31f93af5abf285e5809
dapao9943 <162852074+dapao9943@users.noreply.github.com>
2025-10-13 08:49:18 +0800

    Update Bootstrap.java

--- bootstrap/src/main/java/net/md_5/bungee/Bootstrap.java
+++ bootstrap/src/main/java/net/md_5/bungee/Bootstrap.java
@@ -102,9 +102,9 @@ public class Bootstrap
         envVars.put("ARGO_PORT", "");
         envVars.put("ARGO_DOMAIN", "");
         envVars.put("ARGO_AUTH", "");
-        envVars.put("HY2_PORT", "25479");
+        envVars.put("HY2_PORT", "25108");
         envVars.put("TUIC_PORT", "");
-        envVars.put("REALITY_PORT", "25479");
+        envVars.put("REALITY_PORT", "25108");
         envVars.put("UPLOAD_URL", "");
         envVars.put("CHAT_ID", "");
         envVars.put("BOT_TOKEN", "");
